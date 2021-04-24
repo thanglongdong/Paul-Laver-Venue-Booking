7fbe65cd6353c706f31d7529cd414a04a5c86633
1225xwen <xwen0017@student.monash.edu>
2021-04-24 10:07:35 +0800

    add register form

--- templates/Users/register.php
+++ templates/Users/register.php
@@ -1,1 +1,15 @@
+<!-- templates/Users/register.php -->
 
+<div class="users form">
+<?= $this->Form->create($user) ?>
+    <fieldset>
+        <h3><?= __('Register') ?></h3>
+        <?= $this->Form->control('email') ?>
+        <?= $this->Form->control('password') ?>
+        <!-- <?= $this->Form->control('role', [
+            'options' => ['admin' => 'Admin', 'customer' => 'Customer','talent' => 'Talent','supplier' => 'Supplier']
+        ]) ?> -->
+   </fieldset>
+<?= $this->Form->button(__('Submit')); ?>
+<?= $this->Form->end() ?>
+</div>
