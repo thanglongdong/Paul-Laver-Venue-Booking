9ee39dd56e8aab495794513892662f5dc81ed32e
1225xwen <xwen0017@student.monash.edu>
2021-04-27 19:37:44 +0800

    addUnauthenticatedActions for Profile

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -60,7 +60,7 @@ class AppController extends Controller
         parent::beforeFilter($event);
         // for all controllers in our application, make index and view
         // actions public, skipping the authentication check.
-        $this->Authentication->addUnauthenticatedActions(['login','register','display']);
+        $this->Authentication->addUnauthenticatedActions(['login','register','display','profile']);
 
 }
 }
