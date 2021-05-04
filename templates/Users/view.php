9bbe40226a74d9573f65b508c4cd44fc3027b245
1225xwen <xwen0017@student.monash.edu>
2021-05-04 12:23:13 +0800

    update user files

--- templates/Users/view.php
+++ templates/Users/view.php
@@ -1,29 +1,29 @@
 <?php
 /**
  * @var \App\View\AppView $this
- * @var \App\Model\Entity\Users $users
+ * @var \App\Model\Entity\User $user
  */
 ?>
 <div class="row">
     <div class="column-responsive column-80">
         <div class="customers view content">
-            <h3><?= h($users->id) ?></h3>
+            <h3><?= h($user->id) ?></h3>
             <table>
                 <tr>
                     <th><?= __('Email') ?></th>
-                    <td><?= h($users->email) ?></td>
+                    <td><?= h($user->email) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Password') ?></th>
-                    <td><?= h($users->password) ?></td>
+                    <td><?= h($user->password) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Role') ?></th>
-                    <td><?= h($users->role) ?></td>
+                    <td><?= h($user->role) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($users->id) ?></td>
+                    <td><?= $this->Number->format($user->id) ?></td>
                 </tr>
             </table>
     </div>
